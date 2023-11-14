130fc852bab25c7ebcee163d942170c9c0aa82c1
Victoria Martinez <martinezvictoria781@gmail.com>
2023-11-14 22:33:41 +0000

    minor change

--- script.js
+++ script.js
@@ -176,15 +176,6 @@ function intersects(obstacle) {
   return areaX * areaX + areaY * areaY <= obstacle.r * obstacle.r;
 }
 
-Array.prototype.move = function (old_index, new_index) {
-  if (new_index >= this.length) {
-      var k = new_index - this.length;
-      while ((k--) + 1) {
-          this.push(undefined);
-      }
-  }
-  this.splice(new_index, 0, this.splice(old_index, 1)[0]);
-};
 draw();
 
 function update() {
