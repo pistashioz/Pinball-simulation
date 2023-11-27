5467df8452a8290a1649433a9e6b7ecde19c1249
Victoria <martinezvictoria781@gmail.com>
2023-11-27 23:33:21 +0000

    created functions to drag box obstacles to canvas

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function myFunction() {
+//dropdown
+
+function showToggle() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
@@ -17,26 +19,26 @@ window.onclick = function(event) {
 }
 
 
-
 // get the pinball canvas and context
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const W = canvas.width, H = canvas.height;
+//get the obstacle box canvas and context
+const canvasObs = document.getElementById('canvasObs');
+const ctxObs = canvasObs.getContext('2d')
+const WObs = canvasObs.width, HObs = canvasObs.height;
+
 let isMouseDown = false;
 let focused = { state: false, key: null };
 let pause = false
 let obstaclesInCanvas = []
 
+
+
 //sound effects
 const bounceSound = new Audio("assets/audio/jump.wav");
 const bounceFlippers = new Audio("assets/audio/jumpFlipper.wav")
 const grabObstacles = new Audio('assets/Audio/grab.wav')
-
-
-//get the obstacles canvas and context
-const canvasObstacles = document.getElementById("canvasObstacles");
-const ctxObstacles = canvas.getContext("2d");
-const WObs = canvas.width, HObs = canvas.height;
 
 
 //Classes 
@@ -89,36 +91,64 @@ class Obstacle {
     this.shakeFrames = 5;
     this.shakeMagnitude = 5; // Adjust the magnitude of the shake
   }
-
   draw() {
     //Bigger circle
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
-    ctx.fillStyle = this.color; // Green color
-    ctx.fill();
-    ctx.closePath();
-
-    //Smaller circle
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.r / 3, 0, 2 * Math.PI);
-    ctx.fillStyle = "#FFFFFF"; // Green color
-    ctx.fill();
-    ctx.closePath();
-
-    
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+      ctx.fillStyle = this.color; // Green color
+      ctx.fill();
+      ctx.closePath();
+  
+      //Smaller circle
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.r / 3, 0, 2 * Math.PI);
+      ctx.fillStyle = "#FFFFFF"; // Green color
+      ctx.fill();
+      ctx.closePath();  
+  
   }
 
   //shake effect in obstacles
   shake() {
-    if (this.shakeFrames > 0) {
-      // update  x and y coordinates with random values within a specified range
-      this.x += Math.random() * this.shakeMagnitude - this.shakeMagnitude / 2;
-      this.y += Math.random() * this.shakeMagnitude - this.shakeMagnitude / 2;
-      // decrease the number of remaining shake frames
-      this.shakeFrames--;
+      if (this.shakeFrames > 0) {
+        // update  x and y coordinates with random values within a specified range
+        this.x += Math.random() * this.shakeMagnitude - this.shakeMagnitude / 2;
+        this.y += Math.random() * this.shakeMagnitude - this.shakeMagnitude / 2;
+        // decrease the number of remaining shake frames
+        this.shakeFrames--;
     }
+
   }
 }
+
+class ObstacleInBox{
+  constructor(x, y, r, color) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.color = color
+    this.shakeFrames = 5;
+    this.shakeMagnitude = 5; // Adjust the magnitude of the shake
+  }
+  draw() {
+    //Bigger circle
+      ctxObs.beginPath();
+      ctxObs.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+      ctxObs.fillStyle = this.color; // Green color
+      ctxObs.fill();
+      ctxObs.closePath();
+  
+      //Smaller circle
+      ctxObs.beginPath();
+      ctxObs.arc(this.x, this.y, this.r / 3, 0, 2 * Math.PI);
+      ctxObs.fillStyle = "#FFFFFF"; // Green color
+      ctxObs.fill();
+      ctxObs.closePath();  
+  
+  }
+}
+
+
 const leftFlipper = new Flipper(canvas.width * 0.35, canvas.height - 90, 120, 50, 10, 30, 'assets/img/leftFlipper.svg');
 const rightFlipper = new Flipper(canvas.width * 0.65, canvas.height - 95, -120, -50, 10, 30, 'assets/img/rightFlipper.svg');
 const ball = new Ball(canvas.width / 2, 30, 15, 10, 10);
@@ -126,10 +156,15 @@ const ball = new Ball(canvas.width / 2, 30, 15, 10, 10);
 const obstacles = [
   new Obstacle(W / 2, H / 2 - 50, 25, 'red'),
   new Obstacle(W / 2 + 100, H / 2 - 150, 25, 'blue'),
-  new Obstacle(W / 2 - 100, H / 2 - 150, 25, 'green'),
-  new Obstacle(W / 2 + 100, H / 2 + 50, 25, 'purple'),
-  new Obstacle(W / 2 - 100, H / 2 + 50, 25,'yellow'),
-];
+  new Obstacle(W / 2 - 100, H / 2 - 150, 25, 'green')
+]
+//Array with obstacles' in the box values
+const obstaclesInBox = [
+  new ObstacleInBox(W / 2, H / 2, 25, 'purple'),
+  new ObstacleInBox(W / 2 + 100, H / 2 - 150, 25, 'yellow'),
+  new ObstacleInBox(W / 2 - 100, H / 2 - 150, 25, 'orange')
+]
+
 
 //Function to check for collision between the ball and an obstacle
 function checkBallObstacleCollision(ball, obstacle) {
@@ -160,6 +195,44 @@ function draw(){
         obstacle.draw();
       }
 }
+
+
+
+function moveInBox(e) {
+  //check if mouse btn is not pressed
+  if (!isMouseDown) {
+    return;
+  }
+  getMousePositionBox(e);
+
+  //check if an obstacle is focused for movement
+  if (focused.state) {
+    //update the focused obstacle's position to the mouse position
+    obstaclesInBox[focused.key].x = mousePosition.x;
+    obstaclesInBox[focused.key].y = mousePosition.y;
+
+    draw();
+    return;
+  }
+  //if no obstacle is focused
+  for (var i = 0; i < obstaclesInBox.length; i++) {
+    //check if mouse intersects with an obstacle
+    if (intersects(obstaclesInBox[i])) {
+      //play audio if intersection is detected and change focused state to true so we can store the index of the focused obstacle
+      grabObstacles.play()
+      focused.state = true;
+      focused.key = i;
+      //increase the size of the current obstacle to be perceived as we grabbed it
+      obstaclesInBox[i].r = 35;
+      break;
+    }
+  }
+
+  draw();
+}
+
+
+
 
 function move(e) {
   //check if mouse btn is not pressed
@@ -198,20 +271,42 @@ function setDraggable(e) {
   let type = e.type;
   if (type === "mousedown") {
     isMouseDown = true;
+    moveOutsideCanvas(e);
   } else if (type === "mouseup") {
-    
     for (let i = 0; i < obstacles.length; i++) {
       if (intersects(obstacles[i])) {
-        //change to obstacle's normal size after being dragged
         obstacles[i].r = 25;
       }
     }
-    //indicates the mouse button is released
     isMouseDown = false;
     releaseFocus();
   }
 }
 
+function setDraggableBox(e) {
+  let type = e.type;
+  if (type === "mousedown") {
+    isMouseDown = true;
+    moveInBox(e);
+  } else if (type === "mouseup") {
+    for (let i = 0; i < obstaclesInBox.length; i++) {
+      if (intersects(obstaclesInBox[i])) {
+        // Create a new Obstacle based on the properties of the focused obstacle in the box
+        const newObstacle = new Obstacle(obstaclesInBox[i].x * Math.random(), obstaclesInBox[i].y * Math.random(), 25, obstaclesInBox[i].color);
+        //Add the selected obstacle inside obstaces array
+        obstacles.push(newObstacle);
+        // Remove the obstacle from the obstaclesInBox array
+        obstaclesInBox.splice(i, 1);
+
+      }
+      console.log(obstacles)
+      //redraw canvas to show the new obstacle
+      draw()
+    }
+    isMouseDown = false;
+    releaseFocus();
+  }
+}
 
 function releaseFocus(){
   focused.state = false
@@ -219,6 +314,15 @@ function releaseFocus(){
 
 function getMousePosition(e){
   var rect = canvas.getBoundingClientRect();
+  //calculate mouse position relative to the canvas
+  mousePosition = {
+    x: Math.round(e.x - rect.left),
+    y: Math.round(e.y - rect.top)
+  }
+}
+
+function getMousePositionBox(e){
+  var rect = canvasObs.getBoundingClientRect();
   //calculate mouse position relative to the canvas
   mousePosition = {
     x: Math.round(e.x - rect.left),
@@ -237,7 +341,7 @@ function intersects(obstacle) {
 draw();
 function update() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-
+  ctxObs.clearRect(0, 0, canvas.width, canvas.height);
   if (!pause) {
     // Update the x and y ball coordinates
     ball.x += ball.speedX;
@@ -310,8 +414,41 @@ function update() {
   for (const obstacle of obstacles) {
     obstacle.draw();
   }
+  for (const obstacle of obstaclesInBox){
+    obstacle.draw();
+  }
 
   requestAnimationFrame(update);
+}
+
+function moveOutsideCanvas(e) {
+  if (isMouseDown && focused.state) {
+    getMousePosition(e);
+    if (isMouseInsideCanvas()) {
+      obstacles[focused.key].x = mousePosition.x;
+      obstacles[focused.key].y = mousePosition.y;
+    }
+    draw();
+  }
+}
+
+function releaseOutsideCanvas() {
+  if (isMouseDown && focused.state) {
+    if (isMouseInsideCanvas()) {
+      obstacles[focused.key].r = 25;
+    }
+    isMouseDown = false;
+    releaseFocus();
+  }
+}
+
+function isMouseInsideCanvas() {
+  return (
+    mousePosition.x >= 0 &&
+    mousePosition.x <= W &&
+    mousePosition.y >= 0 &&
+    mousePosition.y <= H
+  );
 }
 
 //pause with the space bar
@@ -346,5 +483,12 @@ document.addEventListener("keyup", (event) => {
 canvas.addEventListener("mousedown", setDraggable);
 canvas.addEventListener("mousemove", move);
 canvas.addEventListener("mouseup", setDraggable);
+
+canvasObs.addEventListener("mousedown", setDraggableBox);
+canvasObs.addEventListener("mousemove", moveInBox);
+canvasObs.addEventListener("mouseup", setDraggableBox);
+
+window.addEventListener("mousemove", moveOutsideCanvas);
+window.addEventListener("mouseup", releaseOutsideCanvas);
 
 update();
